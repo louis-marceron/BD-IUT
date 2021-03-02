f069dba98a32fb199a459a22bbfb826b28505b5b
Louis Marceron <louis.marceron@etu.umontpellier.fr>
2021-03-02 01:17:15 +0100

    R49

--- TD Camping/Exercices.sql
+++ TD Camping/Exercices.sql
@@ -6,68 +6,71 @@ WHERE salaireEmploye > 2000;
 --R12
 SELECT nomCamping
 FROM Campings
-WHERE villeCamping = 'Palavas' AND nbEtoilesCamping = 5;
+WHERE villeCamping = 'Palavas'
+  AND nbEtoilesCamping = 5;
 
 --R13
 SELECT nomEmploye, prenomEmploye
 FROM Employes e
-JOIN Campings c ON e.idCamping = c.idCamping
+         JOIN Campings c ON e.idCamping = c.idCamping
 WHERE c.nomCamping = 'Les Flots Bleus'
 ORDER BY salaireEmploye;
 
 --R14
 SELECT sub.nomEmploye, sub.prenomEmploye
 FROM Employes sub
-JOIN Employes chef ON sub.idEmployeChef = chef.idEmploye
-WHERE chef.prenomEmploye = 'Gaspard' AND chef.nomEmploye = 'Alizan';
+         JOIN Employes chef ON sub.idEmployeChef = chef.idEmploye
+WHERE chef.prenomEmploye = 'Gaspard'
+  AND chef.nomEmploye = 'Alizan';
 
 --R15
 SELECT nomClient, prenomClient
 FROM Clients c
-JOIN Locations l ON c.idClient = l.idClient
-JOIN Bungalows b ON l.idBungalow = b.idBungalow
-JOIN Campings camp ON b.idCamping = camp.idCamping
+         JOIN Locations l ON c.idClient = l.idClient
+         JOIN Bungalows b ON l.idBungalow = b.idBungalow
+         JOIN Campings camp ON b.idCamping = camp.idCamping
 WHERE nomCamping = 'Les Flots Bleus'
-AND dateDebut <= '14/07/2017'
-AND dateFin >= '14/07/2017';
+  AND dateDebut <= '14/07/2017'
+  AND dateFin >= '14/07/2017';
 
 --R16
 SELECT nomClient, prenomClient
 FROM Clients
 WHERE idClient IN (
-SELECT idClient
-FROM Locations l
-JOIN Bungalows b ON l.idBungalow = b.idBungalow
-JOIN Campings c ON b.idCamping = c.idCamping
-WHERE nomCamping = 'Les Flots Bleus'
-AND dateDebut <= '31/07/2017'
-AND dateFin >= '01/07/2017');
+    SELECT idClient
+    FROM Locations l
+             JOIN Bungalows b ON l.idBungalow = b.idBungalow
+             JOIN Campings c ON b.idCamping = c.idCamping
+    WHERE nomCamping = 'Les Flots Bleus'
+      AND dateDebut <= '31/07/2017'
+      AND dateFin >= '01/07/2017');
 
 --R17
 SELECT nomClient, prenomClient
 FROM Clients
 WHERE villeClient IN (
-SELECT villeCamping
-FROM Campings);
+    SELECT villeCamping
+    FROM Campings);
 
 --R18
 SELECT COUNT(*) as "Nombre de services"
 FROM Bungalows b
-JOIN Proposer p ON b.idBungalow = p.idBungalow
+         JOIN Proposer p ON b.idBungalow = p.idBungalow
 WHERE nomBungalow = 'Le Titanic';
 
 --R19
 SELECT MAX(salaireEmploye) as "Salaire max"
 FROM Employes e
-JOIN Campings c ON e.idCamping = c.idCamping
+         JOIN Campings c ON e.idCamping = c.idCamping
 WHERE nomCamping = 'Les Flots Bleus';
 
 --R20
 SELECT COUNT(DISTINCT idCamping) as "Nombre de campings fréquentés"
 FROM Clients c
-JOIN Locations l ON c.idClient = l.idClient
-JOIN Bungalows b ON l.idBungalow = b.idBungalow
-WHERE nomClient = 'Zeblouse' AND prenomClient = 'Agathe';
+         JOIN Locations l ON c.idClient = l.idClient
+         JOIN Bungalows b ON l.idBungalow = b.idBungalow
+WHERE nomClient = 'Zeblouse'
+  AND prenomClient = 'Agathe';
 
 --R21
 SELECT nomBungalow
@@ -80,11 +83,12 @@ WHERE superficieBungalow IN (
 --R22
 SELECT nomEmploye, prenomEmploye
 FROM Employes e
-JOIN Campings c ON e.idCamping = c.idCamping
-WHERE nomCamping = 'Les Flots Bleus' AND salaireEmploye IN(
+         JOIN Campings c ON e.idCamping = c.idCamping
+WHERE nomCamping = 'Les Flots Bleus'
+  AND salaireEmploye IN (
     SELECT MIN(salaireEmploye)
     FROM Employes e
-    JOIN Campings c ON e.idCamping = c.idCamping
+             JOIN Campings c ON e.idCamping = c.idCamping
     WHERE nomCamping = 'Les Flots Bleus'
 );
 
@@ -93,11 +97,9 @@ SELECT nomBungalow
 FROM Bungalows
 WHERE idBungalow IN (
     SELECT idBungalow
-    FROM Bungalows
-    MINUS
-    SELECT idBungalow
-    FROM Proposer
-);
+    FROM Bungalows MINUS
+SELECT idBungalow
+FROM Proposer );
 --ou
 SELECT nomBungalow
 FROM Bungalows
@@ -111,25 +113,22 @@ SELECT nomEmploye, prenomEmploye
 FROM Employes
 WHERE idEmploye IN (
     SELECT idEmploye
-    FROM Employes
-    MINUS
-    SELECT idEmployeChef
-    FROM Employes
-);
+    FROM Employes MINUS
+SELECT idEmployeChef
+FROM Employes );
 
 --R25
 SELECT nomBungalow
 FROM Bungalows
 WHERE idBungalow IN (
     SELECT idBungalow
-    FROM Bungalows
-    MINUS
-    SELECT b.idBungalow
-    FROM Bungalows b
-    JOIN Proposer p ON b.idBungalow = p.idBungalow
-    JOIN Services s ON p.idService = s.idService
-    WHERE nomService = 'Climatisation' OR nomService = 'TV'
-);
+    FROM Bungalows MINUS
+SELECT b.idBungalow
+FROM Bungalows b
+         JOIN Proposer p ON b.idBungalow = p.idBungalow
+         JOIN Services s ON p.idService = s.idService
+WHERE nomService = 'Climatisation'
+   OR nomService = 'TV' );
 
 --R26
 SELECT nomService
@@ -137,14 +136,14 @@ FROM Services
 WHERE idService IN (
     SELECT idService
     FROM Proposer p
-    JOIN Bungalows b ON p.idBungalow = b.idBungalow
-    JOIN Campings c ON b.idCamping = c.idCamping
+             JOIN Bungalows b ON p.idBungalow = b.idBungalow
+             JOIN Campings c ON b.idCamping = c.idCamping
     WHERE nomCamping = 'La Décharge Monochrome'
     INTERSECT
     SELECT idService
     FROM Proposer p
-    JOIN Bungalows b ON p.idBungalow = b.idBungalow
-    JOIN Campings c ON b.idCamping = c.idCamping
+             JOIN Bungalows b ON p.idBungalow = b.idBungalow
+             JOIN Campings c ON b.idCamping = c.idCamping
     WHERE nomCamping = 'The White Majestic'
 );
 
@@ -154,25 +153,26 @@ FROM Clients
 WHERE idClient IN (
     SELECT idClient
     FROM Locations l
-    JOIN Bungalows b ON l.idBungalow = b.idBungalow
-    JOIN Campings c ON b.idCamping = c.idCamping
-    WHERE nomCamping = 'La Décharge Monochrome' OR nomCamping = 'The White Majestic'
+             JOIN Bungalows b ON l.idBungalow = b.idBungalow
+             JOIN Campings c ON b.idCamping = c.idCamping
+    WHERE nomCamping = 'La Décharge Monochrome'
+       OR nomCamping = 'The White Majestic'
 )
 ORDER BY nomClient, prenomClient;
 
 --R28
 SELECT nomEmploye, prenomEmploye, nomCamping
 FROM Employes e
-LEFT JOIN Campings c ON e.idCamping = c.idCamping
+         LEFT JOIN Campings c ON e.idCamping = c.idCamping
 ORDER BY nomEmploye, prenomEmploye;
 
 --R29
 SELECT nomClient, prenomClient
 FROM Client c
-JOIN Locations l ON c.idClient = l.idClient
-JOIN Bungalows b ON l.idBungalow = b.idBungalow
-JOIN Campings c ON b.idCamping = c.idCamping
-WHERE
+         JOIN Locations l ON c.idClient = l.idClient
+         JOIN Bungalows b ON l.idBungalow = b.idBungalow
+         JOIN Campings c ON b.idCamping = c.idCamping
+WHERE;
 
 --R3A
 SELECT nomEmploye, prenomEmploye
@@ -184,11 +184,9 @@ SELECT nomBungalow
 FROM Bungalows
 WHERE idBungalow IN (
     SELECT idBungalow
-    FROM Bungalows
-    MINUS
-    SELECT idBungalow
-    FROM Locations
-);
+    FROM Bungalows MINUS
+SELECT idBungalow
+FROM Locations );
 --ou
 SELECT nomBungalow
 FROM Bungalows
@@ -199,20 +197,20 @@ WHERE idBungalow NOT IN (
 --ou
 SELECT nomBungalow
 FROM Bungalows b
-WHERE NOT EXISTS (
-    SELECT *
-    FROM Locations l
-    WHERE b.idBungalow = l.idBungalow
-);
+WHERE NOT EXISTS(
+        SELECT *
+        FROM Locations l
+        WHERE b.idBungalow = l.idBungalow
+    );
 
 --R30
 SELECT nomCamping
 FROM Campings c
-WHERE NOT EXISTS (
-    SELECT *
-    FROM Employes e
-    WHERE c.idCamping = e.idCamping
-);
+WHERE NOT EXISTS(
+        SELECT *
+        FROM Employes e
+        WHERE c.idCamping = e.idCamping
+    );
 --ou (ne marche pas car il y a des NULL dans la table Employes)
 SELECT nomCamping
 FROM Campings
@@ -224,22 +222,22 @@ WHERE idCamping NOT IN (
 --R31
 SELECT COUNT(idBungalow) as "Nombre de bungalows"
 FROM Bungalows b
-WHERE NOT EXISTS (
-    SELECT *
-    FROM Proposer p
-    WHERE b.idBungalow = p.idBungalow
-);
+WHERE NOT EXISTS(
+        SELECT *
+        FROM Proposer p
+        WHERE b.idBungalow = p.idBungalow
+    );
 
 --R32
 SELECT nomClient
 FROM Clients c
-WHERE NOT EXISTS (
-    SELECT *
-    FROM Locations l
-    JOIN Bungalows b ON l.idBungalow = b.idBungalow
-    WHERE c.idClient = l.idClient
-     AND b.superficieBungalow < 58 )
-AND idClient IN (
+WHERE NOT EXISTS(
+        SELECT *
+        FROM Locations l
+                 JOIN Bungalows b ON l.idBungalow = b.idBungalow
+        WHERE c.idClient = l.idClient
+          AND b.superficieBungalow < 58)
+  AND idClient IN (
     SELECT idClient
     FROM Locations
 );
@@ -249,25 +247,157 @@ SELECT nomCamping
 FROM Campings
 WHERE idCamping IN (
     SELECT idCamping
-    FROM Employes
-    MINUS
+    FROM Employes MINUS
+SELECT idCamping
+FROM Employes
+WHERE salaireEmploye < 1000 );
+--or
+SELECT nomCamping
+FROM Campings c
+WHERE NOT EXISTS(
+        SELECT *
+        FROM Campings c2
+                 JOIN Employes e ON c2.idCamping = e.idCamping
+        WHERE c.idCamping = c2.idCamping
+          AND e.salaireEmploye < 1000
+    )
+  AND idCamping IN (
     SELECT idCamping
     FROM Employes
-    WHERE salaireEmploye < 1000
 );
---or
-SELECT nomCaping
-FROM Campings c
-WHERE NOT EXISTS (
 
+--R34
+SELECT nomClient
+FROM Clients c
+WHERE villeClient = 'Montpellier'
+  AND NOT EXISTS(
+        SELECT *
+        FROM Locations l
+        WHERE c.idClient = l.idClient
+          AND NOT EXISTS(
+                SELECT *
+                FROM Proposer p
+                WHERE l.idBungalow = p.idBungalow
+            )
+    );
+
+--R40
+SELECT DISTINCT c.idClient, nomClient, prenomClient
+FROM Clients c
+         JOIN Locations l ON c.idClient = l.idClient
+         JOIN Bungalows b ON l.idBungalow = b.idBungalow
+         JOIN Campings camp ON b.idCamping = camp.idCamping
+WHERE villeClient = villeCamping;
+
+--R41
+SELECT nomClient, prenomClient
+FROM Clients c
+WHERE NOT EXISTS(
+        SELECT idClient
+        FROM Locations l
+                 JOIN Bungalows b ON l.idBungalow = b.idBungalow
+                 JOIN Campings c ON b.idCamping = c.idCamping
+        WHERE nomCamping = 'Les Flots Bleus'
+          AND c.idClient = l.idClient
+    );
+
+--R42
+SELECT COUNT(*)
+FROM Clients c
+         JOIN Locations l ON c.idClient = l.idClient
+         JOIN Bungalows b ON l.idBungalow = b.idBungalow
+         JOIN Campings camp ON b.idCamping = camp.idCamping
+WHERE nomCamping = 'La Décharge Monochrome'
+  AND nomClient = 'Zeblouse'
+  AND prenomClient = 'Agathe';
+
+--R43
+SELECT sub.nomEmploye, sub.prenomEmploye, NVL(chef.nomEmploye, 'Pas de chef')
+FROM Employes sub
+         LEFT JOIN Employes chef ON sub.idEmployeChef = chef.idEmploye
+ORDER BY sub.nomEmploye;
+
+--R44
+SELECT nomBungalow
+FROM Bungalows b
+WHERE NOT EXISTS(
+        SELECT *
+        FROM Proposer p
+                 JOIN Services s ON p.idService = s.idService
+        WHERE b.idBungalow = p.idBungalow
+          AND categorieService = 'Loisir'
+    );
+
+--R45
+SELECT idLocation
+FROM Locations
+WHERE (dateFin - dateDebut) IN (
+    SELECT MAX(dateFin - dateDebut)
+    FROM Locations l
+             JOIN Bungalows b ON l.idLocation = b.idBungalow
+    WHERE nomBungalow = 'The White Majestic');
+
+--R46
+SELECT DISTINCT VilleClient
+FROM Clients c
+WHERE NOT EXISTS(
+        SELECT *
+        FROM Campings camp
+        WHERE c.VilleClient = camp.VilleCamping
+    );
+
+--R47
+SELECT nomClient, prenomClient
+FROM Clients c
+         JOIN Locations l ON c.idClient = l.idClient
+WHERE dateFin IN (
+    SELECT MAX(dateFin)
+    FROM Locations l
+             JOIN Bungalows b ON l.idBungalow = b.idBungalow
+    WHERE nomBungalow = 'La Poubelle'
+);
+
+--R48
+SELECT nomClient, prenomClient
+FROM Clients c1
+WHERE NOT EXISTS(
+        SELECT c2.idClient
+        FROM Clients c2
+                 JOIN Locations l ON c2.idClient = l.idClient
+        WHERE (dateFin - dateDebut) < 10
+          AND c1.idClient = c2.idClient
+    )
+  AND idClient IN (
+    SELECT idClient
+    FROM Locations
 )
+;
 
+--R49
+SELECT nomClient, prenomClient
+FROM Clients
+WHERE idClient IN (
 
-
-
-
-
-
+    SELECT idClient
+    FROM Locations l
+             JOIN Bungalows b ON l.idBungalow = b.idBungalow
+             JOIN Campings c ON b.idCamping = c.idCamping
+    WHERE nomCamping = 'La Décharge Monochrome' OR nomCamping = 'Les Flots Bleus'
+    MINUS
+    (
+    SELECT idClient
+    FROM Locations l
+    JOIN Bungalows b ON l.idBungalow = b.idBungalow
+    JOIN Campings c ON b.idCamping = c.idCamping
+    WHERE nomCamping = 'La Décharge Monochrome'
+    INTERSECT
+    SELECT idClient
+    FROM Locations l
+    JOIN Bungalows b ON l.idBungalow = b.idBungalow
+    JOIN Campings c ON b.idCamping = c.idCamping
+    WHERE nomCamping = 'Les Flots Bleus'
+    )
+    );
 
 
 
